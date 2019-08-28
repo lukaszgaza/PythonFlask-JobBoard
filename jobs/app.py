8a398b146621440e65179188fb037b9affd1c7ea
Lukasz Gaza <lgaza@virtuslab.com>
2019-08-28 18:08:34 +0200

    Changes

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,6 @@
 import datetime
 import sqlite3
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, redirect, url_for
 
 PATH = 'db/jobs.sqlite'
 
@@ -59,4 +59,6 @@ def review(employer_id):
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
                     (review, rating, title, date, status, employer_id),
                     commit=True)
+
+        return redirect(url_for('employer', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
